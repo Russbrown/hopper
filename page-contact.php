2e26b5acf19805c39b6d9e7f3aa0dc66d10f3f5c
Russell Brown <emailrussellbrown@gmail.com>
2017-01-10 20:09:26 +0000

    contact page added

--- page-contact.php
+++ page-contact.php
@@ -11,34 +11,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<section class="hero hero--short">
-				<div class="hero__img hero__img--short"></div>
-				<div class="hero__copy hero__copy--short">
-					<?php the_title( '<h1 class="contact-title">', '</h1>' ); ?>
-					<ul class="icon-bullets">
-						<li><i class="icon-star"></i><?php echo get_field( "bullet1" ); ?></li>
-						<li><i class="icon-star"></i><?php echo get_field( "bullet2" ); ?></li>
-						<li><i class="icon-star"></i><?php echo get_field( "bullet3" ); ?></li>
-						<li><i class="icon-star"></i><?php echo get_field( "bullet4" ); ?></li>			
-					</ul>
-				</div>
-			</section>
-
 			<main id="main" class="contact-page" role="main">
 
-				<div class="contact-page-content">
+				<h1 class="contact__title"><span>Contact</span></h1>
 
-					<?php the_content(); ?>
+				<?php $field = get_fields($post_id); ?>
 
-				</div><!-- .entry-content -->
-
-				<div class="contact-page-form">
-					<?php echo do_shortcode( '[contact-form-7 id="19" title="Contact form 1"]' ); ?>
-				</div>
-
-		<?php endwhile; // End of the loop. ?>
+				<div class="contact__text"><?php echo($field["contact_info"]);?></div>
 
 			</main><!-- #main -->
 	</div><!-- #primary -->
